78822eb68ec536e9b332bb58637a44ea2d00b3d9
Vicente Patricio <viceents@gmail.com>
2020-07-13 12:40:51 -0300

    init

---
+++ TestingScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingScrollView
+//
+//  Created by Vicente Patricio on 12/07/20.
+//  Copyright © 2020 Vicente Patricio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
